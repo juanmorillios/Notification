90fe8a3b66852ad828de154645340991e31705fe
Juan Ml. Morillo <juanmorillios@gmail.com>
2015-07-25 00:39:27 +0200

    Initial Commit

---
+++ Notification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notification
+//
+//  Created by Juan Morillo on 25/7/15.
+//  Copyright (c) 2015 JuanMorillios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Notification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notification
+//
+//  Created by Juan Morillo on 25/7/15.
+//  Copyright (c) 2015 JuanMorillios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
